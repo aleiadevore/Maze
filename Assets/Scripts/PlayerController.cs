14529dc085c756ba8a93bbeb83b57cdd14c9b052
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-04 14:58:24 -0500

    Added script to prefab, player collects coins

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     /// <summary>Sets player's speed</summary>
     public float speed = 5.0f;
     private CharacterController controller;
+    private int score = 0;
 
     ///<summary>Adds an instance of CharacterController to Player at start</summary>
     void Start()
@@ -19,6 +20,17 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * speed);
+    }
+
+    ///<summary>Inriments score when Player touches object called Pickup</summary>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Pickup")
+        {
+            score += 1;
+            Debug.Log($"Score: {score}");
+            Destroy(other.gameObject);
+        }
     }
 
     /// <summary>Destroys controller on disable</summary>
