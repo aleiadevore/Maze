a62d55d105c7841a55542c9883d150b28ffcd2d3
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-04 15:51:09 -0500

    Prints winning message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     /// <summary>Sets player's speed</summary>
-    public float speed = 5.0f;
+    public float speed = 10.0f;
     ///<summary>Set's Players health (default of 5)</summary>
     public int health = 5;
     private CharacterController controller;
@@ -24,7 +24,7 @@ public class PlayerController : MonoBehaviour
         controller.Move(move * Time.deltaTime * speed);
     }
 
-    ///<summary>Inriments score when Player touches object called "Pickup," decriments health when player touches object tagged "Trap"</summary>
+    ///<summary>Inriments score when Player touches object called "Pickup," decriments health when player touches object tagged "Trap." Handles win.</summary>
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag=="Pickup")
@@ -38,6 +38,11 @@ public class PlayerController : MonoBehaviour
         {
             health -= 1;
             Debug.Log($"Health: {health}");
+        }
+
+        if (other.gameObject.tag=="Goal")
+        {
+            Debug.Log("You win!");
         }
     }
 
