5b15dc11e64b436730633aab35e4909879290d16
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-04 12:49:08 -0500

    Fixed NullReferenceException

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     /// <summary>Sets player's speed</summary>
-    public float speed = 2.0f;
+    public float speed = 5.0f;
     private CharacterController controller;
 
-    // Start is called before the first frame update
+    ///<summary>Adds an instance of CharacterController to Player at start</summary>
     void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+    ///<summary>Moves game object when keys WASD or arrow keys are pressed</summary>
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * speed);
+    }
 
-        if (move != Vector3.zero)
-            gameObject.transform.forward = move;
-            gameObject.transform.up = move;
+    /// <summary>Destroys controller on disable</summary>
+    public void OnDisable()
+    {
+        Destroy(controller);
     }
 }
