2b04b6b52107f99b02426ca7c166c5857f98c5b6
Aleia McNaney <aleiamcnaney@Aleias-MacBook-Air.local>
2021-08-04 11:28:15 -0500

    full WASD works

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,10 +17,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * speed);
 
         if (move != Vector3.zero)
             gameObject.transform.forward = move;
+            gameObject.transform.up = move;
     }
 }
